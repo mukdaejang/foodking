79e45679508a123b5eb0d217d35216b0cb4b5aac
JJongBin <gghkdu2@gmail.com>
2022-03-14 11:11:07 +0900

    feat: config and assets

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,34 @@
-const { useBabelRc, removeModuleScopePlugin, override } = require('customize-cra');
+const path = require('path');
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
+const {
+  override,
+  addBabelPreset,
+  addBabelPlugin,
+  addWebpackAlias,
+} = require('customize-cra');
 
-module.exports = override(useBabelRc(), removeModuleScopePlugin());
+const overrideConfig = override(
+  /* webpack 사용자 정의 구성 덮어쓰기 */
+  (config, env) => {
+    /* 개발 모드 */
+    if (env === 'development') {
+    }
+
+    /* 배포 모드 */
+    if (env === 'production') {
+      config.devtool = false;
+      config.plugins = [...config.plugins, new SpeedMeasurePlugin()];
+    }
+
+    return config;
+  },
+
+  addBabelPreset('@emotion/babel-preset-css-prop'),
+  addBabelPlugin('@emotion'),
+
+  addWebpackAlias({
+    '@': path.resolve(__dirname, 'src'),
+  }),
+);
+
+module.exports = overrideConfig;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Restaurants, Category, Carousel } from '@/components';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Restaurants />
+    <Category />
+    <Carousel />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
