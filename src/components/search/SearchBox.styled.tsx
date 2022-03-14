77bd31adc8dc0627900c42ef37a8dc8b429ea54d
Minsung Kim <minsoftk@gmail.com>
2022-03-14 12:04:31 +0900

    feat: seperate css files

---
+++ src/components/search/SearchBox.styled.tsx
@@ -0,0 +1,94 @@
+import { css } from '@emotion/react';
+
+export const searchBar = css`
+  width: 60%;
+  margin: 0 auto;
+  margin-top: 60px;
+`;
+
+export const searchBar__field = css`
+  margin: 0px;
+  padding: 0px;
+
+  height: 54px;
+  background: white;
+
+  margin: 0 auto;
+  border: 1px solid rgba(255, 133, 0, 1);
+  border-radius: 40px;
+`;
+
+export const findImgStyle = css`
+  width: 30px;
+  height: 31px;
+  padding: 30px;
+  opacity: 0.5;
+`;
+
+export const searchBar__contents = css`
+  margin: 0px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .contents__left {
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    align-items: center;
+  }
+
+  .contents__right {
+    width: 200px;
+    height: 100%;
+
+    display: flex;
+    align-items: center;
+  }
+`;
+
+export const search__input = css`
+  width: 70%;
+  height: 100%;
+  font-size: 20px;
+
+  margin: 0;
+  padding: 0;
+  border: 0;
+  :focus {
+    outline: none;
+  }
+`;
+
+export const searh__btn = css`
+  width: 100%;
+  height: 100%;
+  font-size: 20px;
+  color: white;
+  cursor: pointer;
+
+  background-color: #ff7100;
+  outline: none;
+  border: none;
+  border-radius: 40px;
+  :hover {
+    opacity: 0.6;
+  }
+`;
+
+export const none = css`
+  display: none;
+`;
+export const display = css`
+  display: block;
+  opacity: 0.5;
+`;
+
+export const position = css`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
--- src/components/search/SearchBox.tsx
+++ src/components/search/SearchBox.tsx
@@ -1,101 +1,17 @@
 import React from 'react';
 import glassSolid from '@/assets/icons/glass-solid.svg';
 
-import { css, jsx } from '@emotion/react';
+import {
+  searchBar,
+  searchBar__contents,
+  searchBar__field,
+  search__input,
+  searh__btn,
+  position,
+  findImgStyle,
+  display,
+} from './SearchBox.styled';
 import SearchModal from './SearchModal';
-
-const searchBar = css`
-  width: 60%;
-  margin: 0 auto;
-  margin-top: 60px;
-`;
-
-const searchBar__field = css`
-  margin: 0px;
-  padding: 0px;
-
-  height: 54px;
-  background: white;
-
-  margin: 0 auto;
-  border: 1px solid rgba(255, 133, 0, 1);
-  border-radius: 40px;
-`;
-
-const findImgStyle = css`
-  width: 30px;
-  height: 31px;
-  padding: 30px;
-  opacity: 0.5;
-`;
-
-const searchBar__contents = css`
-  margin: 0px;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  .contents__left {
-    width: 100%;
-    height: 100%;
-
-    display: flex;
-    align-items: center;
-  }
-
-  .contents__right {
-    width: 200px;
-    height: 100%;
-
-    display: flex;
-    align-items: center;
-  }
-`;
-
-const search__input = css`
-  width: 70%;
-  height: 100%;
-  font-size: 20px;
-
-  margin: 0;
-  padding: 0;
-  border: 0;
-  :focus {
-    outline: none;
-  }
-`;
-
-const searh__btn = css`
-  width: 100%;
-  height: 100%;
-  font-size: 20px;
-  color: white;
-  cursor: pointer;
-
-  background-color: #ff7100;
-  outline: none;
-  border: none;
-  border-radius: 40px;
-  :hover {
-    opacity: 0.6;
-  }
-`;
-
-const none = css`
-  display: none;
-`;
-const display = css`
-  display: block;
-  opacity: 0.5;
-`;
-
-const position = css`
-  position: relative;
-  width: 100%;
-  height: 100%;
-`;
 
 const SearchBox = () => {
   return (
