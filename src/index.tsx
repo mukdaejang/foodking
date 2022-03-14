60c263773f1d1b843eb5b02b88f3bc8e409f67c8
ywc8851 <ywc8851@naver.com>
2022-03-14 15:39:45 +0900

    docs: connect firebase init #19

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,21 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from '@/App';
-
+import { auth } from './firebase';
+import AuthProvider from '../src/provider/authProvider';
 import { ThemeProvider } from '@emotion/react';
 import GlobalStyle from '@/styles/global';
 import theme from '@/styles/theme';
+
+console.log(auth);
 
 ReactDOM.render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </ThemeProvider>
   </StrictMode>,
   document.getElementById('root'),
