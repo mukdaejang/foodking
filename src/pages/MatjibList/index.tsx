12da9f8cceb360adf794d0232e6170e7128dddfd
h-sick <luna23825@gmail.com>
2022-03-15 11:00:08 +0900

    feat: #33 detail page markup and styling
    - seperate files in SurroundPopular.tsx and SurroundPopulars/index.tsx

--- src/pages/MatjibList/index.tsx
+++ src/pages/MatjibList/index.tsx
@@ -1,18 +1,12 @@
-import styled from '@emotion/styled';
-import { MatjibListTags } from '@/components';
-
-const Title = styled.h1`
-  font-size: 1.3rem;
-  font-weight: 600;
-  margin-bottom: 1rem;
-  color: ${(props) => props.theme.colors.orange};
-`;
+import { MatjibListTags, SurroundPopluars } from '@/components';
+import { Title } from '@/components/style';
 
 const MatjipList = () => {
   return (
     <div>
       <Title>믿고 보는 맛집 리스트</Title>
       <MatjibListTags />
+      <SurroundPopluars />
     </div>
   );
 };
