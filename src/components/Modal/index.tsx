53a2441b11e15568010cb63cf3c121c9eed866e1
ywc8851 <ywc8851@naver.com>
2022-03-16 18:11:00 +0900

    style : change modal component css #53

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -4,26 +4,7 @@ import ModalPortal from './ModalPortal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark, faStar } from '@fortawesome/free-solid-svg-icons';
 import SocialLogin from './SocialLogin';
-import {
-  ModalContainer,
-  modalContent,
-  modalFooter,
-  modalLi,
-  modalLink,
-  modalListDiv,
-  modalListImg,
-  modalListUl,
-  modalNav,
-  modalOpen,
-  modalSection,
-  modalUl,
-  icon,
-  iconX,
-  listInfo,
-  listScore,
-  listTitle,
-  loginButton,
-} from './Modal.styled';
+import { ModalContainer } from './Modal.styled';
 
 type ModalProps = {
   onClickToggleModal: () => void;
@@ -31,12 +12,15 @@ type ModalProps = {
 
 const Modal = ({ onClickToggleModal }: ModalProps) => {
   const [modalOpened, setModalOpened] = useState(false);
+
   const handleOpen = () => {
     setModalOpened(true);
   };
+
   const handleClose = () => {
     setModalOpened(false);
   };
+
   const handleBackClose = () => {
     if (onClickToggleModal) {
       onClickToggleModal();
@@ -46,45 +30,41 @@ const Modal = ({ onClickToggleModal }: ModalProps) => {
   return (
     <div css={ModalContainer}>
       <ModalPortal closePortal={handleBackClose}></ModalPortal>
-      <div css={modalOpen}>
-        <ul css={modalUl}>
-          <li css={modalLi}>최근 본 맛집</li>
-          <li css={modalLi}>가고싶다</li>
+      <div>
+        <ul>
+          <li>최근 본 맛집</li>
+          <li>가고싶다</li>
         </ul>
-        <div css={modalContent}>
-          <div css={modalNav}>
-            <button css={icon} onClick={onClickToggleModal}>
-              창 닫기
-            </button>
-            <button css={icon}>
-              <FontAwesomeIcon icon={faXmark} css={iconX} />
+        <div>
+          <div>
+            <button onClick={onClickToggleModal}>창 닫기</button>
+            <button>
+              <FontAwesomeIcon icon={faXmark} />
               <span>모두 지우기</span>
             </button>
           </div>
-          <ul css={modalListUl}>
+          <ul>
             <li>
-              <section css={modalSection}>
+              <section>
                 <a href="/">
-                  <img src={foodImage} alt="food" css={modalListImg} />
+                  <img src={foodImage} alt="food" />
                 </a>
-                <div css={modalListDiv}>
-                  <a href="/" css={modalLink}>
-                    <h3 css={listTitle}>올레무스</h3>
-                    <span css={listScore}>4.4</span>
+                <div>
+                  <a href="/">
+                    <h3>올레무스</h3>
+                    <span>4.4</span>
                   </a>
-                  <span css={listInfo}>서대문구 - 카페</span>
+                  <span>서대문구 - 카페</span>
                 </div>
-                <button css={icon}>
+                <button>
                   <FontAwesomeIcon icon={faStar} size="2x" />
                 </button>
               </section>
             </li>
           </ul>
         </div>
-        <footer css={modalFooter}>
-          <button css={loginButton} onClick={handleOpen}>
-            로그인
-          </button>
+        <footer>
+          <button onClick={handleOpen}>로그인</button>
           {modalOpened && (
             <ModalPortal closePortal={handleClose}>
               <SocialLogin closePortal={handleClose}></SocialLogin>
