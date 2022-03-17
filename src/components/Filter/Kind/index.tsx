10e851972eeb8240007922357280ef6cf3fc2980
Minsung Kim <minsoftk@gmail.com>
2022-03-17 20:37:01 +0900

    feat: #47 delete no use module
    - delete modules in Kind, Local.styled

--- src/components/Filter/Kind/index.tsx
+++ src/components/Filter/Kind/index.tsx
@@ -1,12 +1,6 @@
 import {} from './Kind.styled';
 import { Section, SubTitle } from '../Filter.styled';
 import { Kind, Label, Span, Input, Img } from './Kind.styled';
-import kor from '../../../assets/icons/kind-kor.svg';
-import japan from '../../../assets/icons/kind-japan.svg';
-import china from '../../../assets/icons/kind-china.svg';
-import pizza from '../../../assets/icons/kind-pizza.svg';
-import coffee from '../../../assets/icons/kind-coffee.svg';
-import beer from '../../../assets/icons/kind-beer.svg';
 
 const FoodKind = () => {
   const foodKind = ['한식', '일식', '중식', '양식', '카페', '주점'];
