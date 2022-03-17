98022f4fcf0b1b2636aff628f57fbe66775ca79f
Minsung Kim <minsoftk@gmail.com>
2022-03-17 20:22:34 +0900

    feat: #47 Filter Component / Local part
    - add Local components
    - Local CSS

---
+++ src/components/Filter/Local/index.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { Section, SubTitle } from '../Filter.styled';
+import { Region, City, Label, Input } from './Local.styled';
+
+const Local = () => {
+  const seoul = [
+    '강남',
+    '삼청',
+    '건대',
+    '가로수길',
+    '홍대',
+    '이태원',
+    '신촌',
+    '종로',
+  ];
+  const kyungido = ['가평군', '수원시', '성님시'];
+  const incheon = ['인천 동구', '인천 부평구'];
+  const busan = ['부산 해운대구', '부산 서구', '부산 남구'];
+
+  const [curmetro, setCurMetro] = useState(seoul);
+  const onClick = (e: any) => {
+    console.log(e.target.classList);
+  };
+  return (
+    <section css={Section}>
+      <SubTitle>지역</SubTitle>
+      <p css={Region} onClick={onClick}>
+        <a href="#" data-filter="seoul" className="selected">
+          서울
+        </a>
+        <a href="#" data-filter="kyungido">
+          경기도
+        </a>
+        <a href="#" data-filter="incheon">
+          인천
+        </a>
+        <a href="#" data-filter="busan">
+          부산
+        </a>
+        <a href="#" data-filter="more">
+          더보기
+        </a>
+      </p>
+      <div css={City}>
+        {curmetro.map((city) => {
+          return (
+            <li key={city}>
+              {/* 체크박스 선택시 로직 추가해야함 */}
+              <label htmlFor={city} css={Label}>
+                {city}
+              </label>
+              <input id={city} value={city} type="checkbox" css={Input}></input>
+            </li>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Local;
