44264d6538e31f974d7fba5d5ba117dcbd814087
Minsung Kim <minsoftk@gmail.com>
2022-03-17 20:24:09 +0900

    feat: #47 Filter Component / Kind part
    - add Kind Component
    - add Kind CSS
    - add food image

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import { Container } from './Filter.styled';
 import Sort from './Sort';
+import Local from './Local';
+import FoodKind from './Kind';
 const Filter = () => {
   return (
     <div css={Container}>
       <Sort></Sort>
+      <Local></Local>
+      <FoodKind></FoodKind>
     </div>
   );
 };
