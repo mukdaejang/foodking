448951cc2040558c5bc3282ee4e356eacb3e5f49
Minsung Kim <minsoftk@gmail.com>
2022-03-17 12:02:40 +0900

    feat: #47 make Filter Component
    - add Filter Component on SearchResult page

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container } from './Filter.styled';
+import Sort from './Sort';
 const Filter = () => {
-  console.log(window.location);
   return (
-    <>
-      <h2>검색 필터 설정</h2>
-      <section>
-        <label htmlFor="sort-food">정렬</label>
-        <input id="sort-food" type="radio"></input>
-      </section>
-    </>
+    <div css={Container}>
+      <Sort></Sort>
+    </div>
   );
 };
 
