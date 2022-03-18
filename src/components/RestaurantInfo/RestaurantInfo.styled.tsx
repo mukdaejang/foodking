a4f3b49832e00221a3fd61b858a02d2eb04e906d
h-sick <luna23825@gmail.com>
2022-03-18 11:58:12 +0900

    feat: #59 Detail Page/Restaurant-Info markup and styling
    - 리뷰쓰기/가고싶다 컴포넌트 미비로 아직 사용안함

--- src/components/RestaurantInfo/RestaurantInfo.styled.tsx
+++ src/components/RestaurantInfo/RestaurantInfo.styled.tsx
@@ -30,6 +30,8 @@ export const TitleHeader = styled.header<{ status: Status }>`
   }
 
   div:last-of-type {
+    font-size: 0.9rem;
+
     span {
       &::after {
         margin-left: 0.4rem;
@@ -48,5 +50,75 @@ export const TitleHeader = styled.header<{ status: Status }>`
         content: '${({ status }) => status.star.toLocaleString()}';
       }
     }
+  }
+
+  padding-bottom: 1.5rem;
+  margin-bottom: 1.5rem;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray400};
+`;
+
+export const Descriptions = styled.div`
+  font-size: 0.8rem;
+
+  dl {
+    display: flex;
+    margin-bottom: 1rem;
+  }
+
+  dt {
+    width: 8rem;
+    color: ${({ theme }) => theme.colors.gray100};
+  }
+
+  dd {
+    & > span {
+      display: block;
+
+      &:last-of-type {
+        margin-top: 0.5rem;
+      }
+
+      &.address_jibun {
+        color: ${({ theme }) => theme.colors.gray300};
+
+        &::before {
+          content: '지번';
+          border: 1px solid ${({ theme }) => theme.colors.gray400};
+          border-radius: 3px;
+          margin-right: 0.5rem;
+          font-size: 0.6rem;
+          padding: 0.1rem;
+        }
+      }
+    }
+
+    ul.menus {
+      width: 16rem;
+      margin-bottom: 1rem;
+
+      li {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-top: 0.5rem;
+        padding-bottom: 0.4rem;
+        border-bottom: 1px solid ${({ theme }) => theme.colors.gray500};
+      }
+    }
+
+    button {
+      width: 4rem;
+      height: 4rem;
+
+      & > img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+
+  p {
+    text-align: right;
+    color: ${({ theme }) => theme.colors.gray100};
   }
 `;
--- src/components/RestaurantInfo/index.tsx
+++ src/components/RestaurantInfo/index.tsx
@@ -1,6 +1,7 @@
-import { TitleHeader } from './RestaurantInfo.styled';
+import { TitleHeader, Descriptions } from './RestaurantInfo.styled';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faPen, faStar } from '@fortawesome/free-solid-svg-icons';
+import foodImage from '@/assets/img/food.jpg';
 
 const RestaurantInfo = () => {
   const status = {
@@ -8,6 +9,22 @@ const RestaurantInfo = () => {
     write: 114,
     star: 3787,
   };
+
+  const restaurantInfo = {
+    address: [
+      '서울특별시 용산구 한강대로38가길 7-2',
+      '서울시 용산구 한강로2가 153-8',
+    ],
+  };
+
+  const menus = [
+    { name: '브라운돈가스', price: 10000 },
+    { name: '브라운치킨가스', price: 10000 },
+    { name: '로스가스', price: 10000 },
+    { name: '화이트돈가스', price: 13000 },
+    { name: '크림우동', price: 9000 },
+  ].map((menu, id) => ({ ...menu, id }));
+
   return (
     <div>
       <TitleHeader status={status}>
@@ -33,6 +50,74 @@ const RestaurantInfo = () => {
           </span>
         </div>
       </TitleHeader>
+      <Descriptions>
+        <dl>
+          <dt>주소</dt>
+          <dd>
+            <span>{restaurantInfo.address[0]}</span>
+            {restaurantInfo.address[1] ? (
+              <span className="address_jibun">{restaurantInfo.address[1]}</span>
+            ) : (
+              ''
+            )}
+          </dd>
+        </dl>
+        <dl>
+          <dt>전화번호</dt>
+          <dd>02-796-2461</dd>
+        </dl>
+        <dl>
+          <dt>음식 종류</dt>
+          <dd>까스 요리</dd>
+        </dl>
+        <dl>
+          <dt>가격대</dt>
+          <dd>만원 미만</dd>
+        </dl>
+        <dl>
+          <dt>주차</dt>
+          <dd>주차공간없음</dd>
+        </dl>
+        <dl>
+          <dt>영업시간</dt>
+          <dd>
+            <span>월~금: 11:30 - 20:00</span>
+            <span>토: 11:30 - 15:00</span>
+          </dd>
+        </dl>
+        <dl>
+          <dt>쉬는시간</dt>
+          <dd>13:30 - 17:00</dd>
+        </dl>
+        <dl>
+          <dt>휴일</dt>
+          <dd>일</dd>
+        </dl>
+        <dl>
+          <dt>메뉴</dt>
+          <dd>
+            <ul className="menus">
+              {menus.map(({ name, price }) => (
+                <li>
+                  <span>{name}</span>
+                  <span>{price.toLocaleString()}원</span>
+                </li>
+              ))}
+            </ul>
+            <button>
+              <img src={foodImage} alt={'메뉴'} />
+            </button>
+          </dd>
+        </dl>
+        <p>업데이트: 2018 2.2</p>
+        <dl>
+          <dt>식당 소개</dt>
+          <dd>
+            로스가스, 브라운돈가스, 치킨가스, 가쓰오 우동 등을 파는 돈까스
+            전문점
+          </dd>
+        </dl>
+      </Descriptions>
     </div>
   );
 };
