b806839ea56a8b2380ae62566e55e6e117a237ca
JJongBin <gghkdu2@gmail.com>
2022-03-23 14:18:48 +0900

    feat: use db data restaurants

--- src/components/Restaurants/index.tsx
+++ src/components/Restaurants/index.tsx
@@ -5,33 +5,39 @@ import {
   RestaurantsList,
 } from './restaurants.styled';
 import { getPostDocs } from '@/firebase/request';
+import { useState, useRef, useEffect } from 'react';
 
 interface restaurantChecker {
   title: string;
 }
+export interface infoType {
+  address: string;
+  category: string;
+  name: string;
+  score: number;
+}
 
 const Restaurants = ({ title }: restaurantChecker) => {
-  let restaurants: Array<Object> = [];
+  const [restaurants, setRestaurants] = useState<Array<infoType>>([]);
+  const restaurantsRef = useRef<HTMLUListElement>(null);
 
-  const postData = (async () => {
-    await getPostDocs().then((res) => (restaurants = res));
-    // console.log(restaurants);
-  })();
+  // const postData = async () => {
+  //   await getPostDocs().then((res) => setRestaurants([...res]));
+  //   console.log(restaurants);
+  // };
 
-  console.log(restaurants);
+  useEffect(() => {
+    getPostDocs().then((res) => setRestaurants([...res]));
+  }, []);
+
+  // console.log(restaurants);
   return (
     <RestaurantsContent>
       <RestaurantsTitle>{`평점이 높은 ${title}`}</RestaurantsTitle>
-      <RestaurantsList>
-        {}
-        <Restaurant />
-        <Restaurant />
-        <Restaurant />
-        <Restaurant />
-        <Restaurant />
-        <Restaurant />
-        <Restaurant />
-        <Restaurant />
+      <RestaurantsList ref={restaurantsRef}>
+        {restaurants.map((restaurant) => (
+          <Restaurant key={restaurant.name} info={restaurant} />
+        ))}
       </RestaurantsList>
     </RestaurantsContent>
   );
